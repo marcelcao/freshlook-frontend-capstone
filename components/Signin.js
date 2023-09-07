1ec473c14f6c74c84cf1fe7ee9ee2b5f3a942c25
Mara Caoile <mara.caoile@gmail.com>
2023-09-06 22:18:37 -0500

    styled sign in page

--- components/Signin.js
+++ components/Signin.js
@@ -4,21 +4,41 @@ import { signIn } from '../utils/auth';
 
 function Signin() {
   return (
-    <div
-      className="text-center d-flex flex-column justify-content-center align-content-center"
-      style={{
-        height: '90vh',
-        padding: '30px',
-        maxWidth: '400px',
-        margin: '0 auto',
-      }}
-    >
-      <h1>Hi there!</h1>
-      <p>Click the button below to login!</p>
-      <Button type="button" size="lg" className="copy-btn" onClick={signIn}>
-        Sign In
-      </Button>
-    </div>
+    <>
+      <div>
+        <div className="sign-in-contain">
+          <div className="copy-items">
+            <div className="copy">
+              <h1>icon</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="copy">
+              <h1>icon</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+            <div className="copy">
+              <h1>icon</h1>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+            </div>
+          </div>
+          <div className="sign-in-content">
+            <div className="sign-in-div">
+              <Button type="button" size="lg" className="sign-in-btn" onClick={signIn}>
+                Sign In
+              </Button>
+            </div>
+            <div className="logo-contain">
+              <h1>freshlook</h1>
+              <p>transform your skin</p>
+              <p>one step at a time</p>
+            </div>
+          </div>
+        </div>
+        <footer>
+          View on Github | © 2023 freshlook by marcelcao
+        </footer>
+      </div>
+    </>
   );
 }
 
